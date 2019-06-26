0a2872be6528bd575652b455c7ce263938f8f0eb
Ayaan Abbasi <34920479+mayaanabbasi@users.noreply.github.com>
2019-06-26 19:00:04 +0530

    Updates

---
+++ precache-manifest.e557505f9119c395c9132d1813fbf91d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da2926cd14e993d2937b417834968081",
+    "url": "/train-live-status/index.html"
+  },
+  {
+    "revision": "d5a554317dfea0dffb31",
+    "url": "/train-live-status/static/css/main.8e8c2b4d.chunk.css"
+  },
+  {
+    "revision": "f6ba8c5d6acb26d62ff3",
+    "url": "/train-live-status/static/js/2.2efab6ce.chunk.js"
+  },
+  {
+    "revision": "d5a554317dfea0dffb31",
+    "url": "/train-live-status/static/js/main.b0ec0e6c.chunk.js"
+  },
+  {
+    "revision": "43671c81725c7ea5a0c2",
+    "url": "/train-live-status/static/js/runtime~main.e6774d45.js"
+  },
+  {
+    "revision": "0ecdb8aa8ed0dbe009535c4afed0c524",
+    "url": "/train-live-status/static/media/background.0ecdb8aa.jpg"
+  }
+]);
